38aef6556a884b9305ffec47b4736f012341a299
unknown <ngochuyen211294@gmail.com>
2025-12-20 09:09:30 +0700

    test bot 1

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 require("dotenv").config();
 const { Client, GatewayIntentBits, Events } = require("discord.js");
 
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  console.log('--- Có tín hiệu Ping từ UptimeRobot! ---');
+  res.send('Server is running!');
+});
+const port = process.env.PORT || 3000;
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
 });
