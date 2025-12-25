c37116ab464e0c75eaa0de90a405b010872fd66d
p.n.huyen <p.n.huyen@viscirclevn.com>
2025-12-25 11:52:45 +0700

    cauca 2

--- utils/fish.js
+++ utils/fish.js
@@ -25,7 +25,7 @@ const FISH_LIST = {
         emoji: "🐳", 
         sellPrice: 50, 
         currency: "primo", 
-        chance: 0.02 // 2% xuất hiện
+        chance: 0.005 // 0.5% xuất hiện
     }
 };
 
