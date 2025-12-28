2c5704279ced827c281ab67fe523cec15fd1883c
unknown <ngochuyen211294@gmail.com>
2025-12-28 13:21:58 +0700

    update sell ca 2

--- game/becaHandler.js
+++ game/becaHandler.js
@@ -25,7 +25,7 @@ async function becaHandler(args, message, fishTank, tankKey, userId) {
             fishTank.forEach(id => {
                 const f = FISH_LIST[id];
                 // Chỉ bán cá thường, GIỮ LẠI cá hiếm (Cá Voi, Cá Mập) hoặc cá có giá > 500
-                if (f && f.currency != 'primo' && f.sellPrice > 10000) {
+                if (f && (f.currency != 'primo' || f.sellPrice > 10000)) {
                     totalMora += f.sellPrice;
                     soldCount++;
                 } else {
