bf7ad5c493ec39d2bca633501f5d41568dd3a8e6
p.n.huyen <p.n.huyen@viscirclevn.com>
2026-01-05 09:59:21 +0700

    update rs time

--- utils/constant.js
+++ utils/constant.js
@@ -4,14 +4,18 @@ const DEVELOPER_IDS = ['1446889473374683400'];
 const MAX_LOVE_POINTS_PER_DAY = 500;
 
 function getCustomDate() {
-    // Lấy thời gian hiện tại theo miliseconds
-    const now = new Date();
-    // Trừ đi 4 tiếng (4 * 60 * 60 * 1000 ms)
-    const customDate = new Date(now.getTime() - (4 * 60 * 60 * 1000));
-    // Trả về định dạng YYYY-MM-DD (Ví dụ: 2024-05-20)
-    // Lúc này, nếu là 3h sáng ngày 20, nó vẫn sẽ trả về ngày 19. 
-    // Đúng 4h sáng ngày 20 mới bắt đầu trả về ngày 20.
-    return customDate.toISOString().split('T')[0];
+   const vnTime = new Date(new Date().toLocaleString("en-US", {timeZone: "Asia/Ho_Chi_Minh"}));
+    
+    // 2. Trừ đi 4 tiếng để tạo mốc reset là 4h sáng
+    // Nếu bây giờ là 3h sáng ngày 20 -> lùi 4h sẽ thành 23h ngày 19 (Chính xác)
+    vnTime.setHours(vnTime.getHours() - 4);
+    
+    // 3. Lấy định dạng YYYY-MM-DD
+    const year = vnTime.getFullYear();
+    const month = String(vnTime.getMonth() + 1).padStart(2, '0');
+    const day = String(vnTime.getDate()).padStart(2, '0');
+    
+    return `${year}-${month}-${day}`;
 }
 
 module.exports={
